9f43a57302e5bdb22d04dfc5fffa8db55aeb102d
Liam Graham <liam.graham@chde.qld.gov.au>
2022-08-24 12:26:35 +1000

    Fix ProfileNav fragment key error

--- src/components/ProfileNav.js
+++ src/components/ProfileNav.js
@@ -1,5 +1,6 @@
 import { getIds, getProfile } from '../data/profiles.js';
 import { Link } from "react-router-dom";
+import { Fragment } from 'react';
 
 function handleScroll(event) {
   console.log(event)
@@ -10,17 +11,17 @@ export default function ProfileNav(props) {
   const items = ids.map((id, index) => {
     const profile = getProfile(id);
     return (
-      <>
-        <Link key={id} className='nav-item' to={id}>
+      <Fragment key={id}>
+        <Link className='nav-item' to={id}>
           {profile.name}
         </Link>
         {
           index < ids.length - 1 &&
-          <span key={`sep-${index}`} className='no-select'>
+          <span className='no-select'>
             •
           </span>
         }
-      </>
+      </Fragment>
     )
   })
 
